9cbeaf95a1c2744a1b8cfe72aa1c7966fb22f579
Suhaib King <suhaibking310@gmail.com>
2025-02-15 21:47:15 +0530

    removed all file

--- configs/schema.js
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, varchar, boolean } from "drizzle-orm/pg-core";
-
-export const userTable = pgTable("users", {
-  id: serial().primaryKey(),
-  userName: varchar(255).notNull(),  // Specify the length
-  email: varchar(255).notNull(),     // Specify the length
-  isMember: boolean().default(false)
-});
--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./configs/schema.js",
-  dbCredentials: {
-    url: "postgresql://neondb_owner:kF36XWBCSJti@ep-long-glitter-a5aa3xd3.us-east-2.aws.neon.tech/neondb?sslmode=require",
-  }
-});
